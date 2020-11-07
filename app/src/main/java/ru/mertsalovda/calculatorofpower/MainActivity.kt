66bd9bd61332f9a1a3eb820380fb269746ab5bb5
Dmitriy Mertsalov <korranhorn009@gmail.com>
2020-11-07 11:44:46 +0300

    added fr_converter.xml layout
    - Добавлена вёрстка фрагмента конвертера.

--- app/src/main/java/ru/mertsalovda/calculatorofpower/MainActivity.kt
+++ app/src/main/java/ru/mertsalovda/calculatorofpower/MainActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fr_basic_calculator)
+        setContentView(R.layout.fr_converter)
     }
 }
