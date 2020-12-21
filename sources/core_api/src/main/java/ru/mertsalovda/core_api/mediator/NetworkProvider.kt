129a12e9f9eaf35a78cbbc44fc19e3ad84ff672e
mertsalovda <damercalov@lotus.bank.srv>
2020-12-21 13:36:29 +0300

    added functions in interface NetworkProvider
    - Добавлены функции в интерфейс NetworkProvider

---
+++ sources/core_api/src/main/java/ru/mertsalovda/core_api/mediator/NetworkProvider.kt
@@ -0,0 +1,14 @@
+package ru.mertsalovda.core_api.mediator
+
+import ru.mertsalovda.core_api.network.CountriesApi
+import ru.mertsalovda.core_api.network.ForeignExchangeRatesApi
+import ru.mertsalovda.core_api.network.NewtonApi
+
+interface NetworkProvider {
+
+    fun provideCountriesApi(): CountriesApi
+
+    fun provideNewtonApi(): NewtonApi
+
+    fun provideForeignExchangeRatesApi(): ForeignExchangeRatesApi
+}
