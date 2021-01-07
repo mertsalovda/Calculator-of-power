da5861be9da3de3fd502ec0f7760494ef4e8d75c
mertsalovda <korranhorn009@gmail.com>
2021-01-07 11:01:36 +0300

    added zoom buttons on GraphFragment.kt
    - Добавлены кнопки управления масштабом на экран GraphFragment.

--- sources/feature-graph/src/main/java/ru/mertsalovda/feature_graph/GraphFragment.kt
+++ sources/feature-graph/src/main/java/ru/mertsalovda/feature_graph/GraphFragment.kt
@@ -5,19 +5,39 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import ru.mertsalovda.feature_graph.databinding.FrGraficBinding
 
 class GraphFragment : Fragment() {
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = GraphFragment()
+    }
+
+    private lateinit var binding: FrGraficBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fr_grafic, container, false)
+    ): View {
+        val view = inflater.inflate(R.layout.fr_grafic, container, false)
+        binding = FrGraficBinding.bind(view)
+        return binding.root
     }
 
-    companion object {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        setListeners()
+    }
 
-        @JvmStatic
-        fun newInstance() = GraphFragment()
+    private fun setListeners() {
+        binding.zoomIn.setOnClickListener {
+            val scale = binding.graphView.scale
+            binding.graphView.setScale(scale + 0.1f)
+        }
+
+        binding.zoomOut.setOnClickListener {
+            val scale = binding.graphView.scale
+            binding.graphView.setScale(scale - 0.1f)
+        }
     }
 }
